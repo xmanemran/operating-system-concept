70e0a4ee1dbe92376f39c97dfad86bd903b37d1b
Emran <xmanemran@gmail.com>
2020-10-12 11:26:50 +0600

    add 3.23 - server

---
+++ 3. Process/programming-problem/3.23/Server.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+
+    String[] quote = {
+            "Never do today what you can do tomorrow",
+            "Nobody lies on the internet",
+            "The cake is a lie"
+    };
+
+    public static void main(String[] args) {
+        try{
+            ServerSocket socket = new ServerSocket(6017);
+
+            while(true){
+                Main main = new Main();
+                Socket client = socket.accept();
+                PrintWriter printWriter = new PrintWriter(client.getOutputStream(), true);
+                printWriter.println(main.getRandomQuote());
+                client.close();
+            }
+
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    int getRandomNumber(){
+        return ThreadLocalRandom.current().nextInt(0, this.quote.length - 1);
+    }
+
+    String getRandomQuote(){
+        return this.quote[this.getRandomNumber()];
+    }
+}
