d43dcb55834bbef0887df71d08b052c3d99d64f0
Emran <xmanemran@gmail.com>
2021-03-28 01:01:02 +0600

    Create 1.hello.c

---
+++ 2. Structure/1.hello.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+static int hello_init(void){
+  printk(KERN_ALERT "Hello module\n");
+  return 0;
+}
+
+static void hello_exit(void){
+  printk(KERN_ALERT "Goodby, cruel world\n");
+}
+
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("Dual BSD/GPL");
